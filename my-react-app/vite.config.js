628d359a019e89764a104ea2510fb692321f3031
Tech & Faith <47467793+agentggg@users.noreply.github.com>
2026-01-04 20:09:20 -0500

    update
    update

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(
-  {
+export default defineConfig({
   plugins: [react()],
-  base: "/",
-  server: {
-    allowedHosts: [
-      '90c4428a4ad4.ngrok-free.app',
-    ]
-  }
+  base: "/Flashcard-Frontend/",
 });
