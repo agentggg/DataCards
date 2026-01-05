eed95cdb535b5766d75f01ff36bd71d4a7d83d93
Tech & Faith <47467793+agentggg@users.noreply.github.com>
2026-01-04 20:26:49 -0500

    pushing to github using automated feature via vscode by Stevenson Gerard

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./App.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Flashcard-Frontend">
       <App />
     </BrowserRouter>
   </React.StrictMode>
